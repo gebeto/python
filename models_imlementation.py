caab55d6715c6537bcf0b1a26aaec4890ea0f332
gebeto <slavik.nychkalo@gmail.com>
2019-04-01 23:05:41 +0300

    import models implementation

--- models_imlementation.py
+++ models_imlementation.py
@@ -1,64 +1,72 @@
-class Field(object):
+class Column(object):
 	_counter = 0
 
-	def __init__(self, formatter = lambda x: x):
-		self.counter = Field._counter
+	def __init__(self, key, col_name, formatter = lambda x: x, *args, **kwargs):
+		self.counter = Column._counter
 		self.formatter = formatter
-		Field._counter += 1
+		self.col_name = col_name
+		self.key = key
+		Column._counter += 1
 
 	def process(self, d):
 		return self.formatter(d)
 
-class Integer(Field):
-	pass
+class Integer(Column):
+	def __init__(self, *args, **kwargs):
+		super(Integer, self).__init__(*args, **kwargs)
+		self.type = 'int'
 
-class String(Field):
-	pass
+class Varchar(Column):
+	def __init__(self, *args, **kwargs):
+		super(Varchar, self).__init__(*args, **kwargs)
+		self.type = 'varchar'
 
-class Real(Field):
-	pass
-
-class Boolean(Field):
-	pass
-
-
+class Real(Column):
+	def __init__(self, *args, **kwargs):
+		super(Real, self).__init__(*args, **kwargs)
+		self.type = 'real'
+		
+class Boolean(Column):
+	def __init__(self, *args, **kwargs):
+		super(Boolean, self).__init__(*args, **kwargs)
+		self.type = 'boolean'
+		
 
 class Model(object):
 	@property
 	def fields(self):
 		items = vars(self.__class__).items()
-		filtered_items = [(key, cls) for key, cls in items if isinstance(cls, Field)]
+		filtered_items = [(key, cls) for key, cls in items if isinstance(cls, Column)]
 		sorted_items = sorted(filtered_items, key=lambda a: a[1])
 		return sorted_items
 
-	def process(self, d):
+	def _process(self, d):
 		fields = self.fields
 		processed = {}
 		for key, field in fields:
 			if not key in d: continue
-			processed[key] = field.process(d[key])
+			processed[field.key] = field.process(d[key])
 		return processed
+
+	def add_row(self, row):
+		print self._process(row)
 	
 
 class Test1(Model):
-	CCC = Integer(formatter=lambda x: x.upper())
-	BBB = Integer()
-	AAA = String()
-
-class Test2(Model):
-	BBB = Integer()
-	CCC = Integer()
-	AAA = String()
-
-
-
+	CCC = Integer('name', 'Name', formatter=lambda x: x.upper())
+	BBB = Integer('sname', 'SecondName')
+	AAA = Varchar('share', 'SirWhore')
 
 
 a = Test1()
-res = a.process({
+a.add_row({
 	"AAA": "Hello",
 	"CCC": "Hello",
 	"BBB": "Hello",
 })
 
-print res
+a.add_row({
+	"AAA": "Ama",
+	"CCC": "Ama",
+	"BBB": "Ama",
+})
