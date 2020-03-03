303a9eae1766ba28f99c7f24d1aa2400ccfdf7e0
gebeto <slavik.nychkalo@gmail.com>
2020-03-03 07:47:43 +0200

    add dbinfo model

--- launchpadr/database.py
+++ launchpadr/database.py
@@ -6,9 +6,10 @@ import os
 
 url_root = os.popen("getconf DARWIN_USER_DIR").read().strip()
 url = os.path.join(url_root, "com.apple.dock.launchpad", "db", "db")
-print(url)
+print(" >>>", url)
 # exit()
 engine = create_engine(f"sqlite:///{url}")
+# engine = create_engine(url)
 factory = sessionmaker()
 factory.configure(bind=engine)
 session = factory()
@@ -30,6 +31,19 @@ class Apps(Base):
 	def __repr__(app):
 		return f"App({app.item_id}, '{app.title}', '{app.bundleid}')"
 
+class DBInfo(Base):
+	__tablename__ = 'dbinfo'
+	key = Column(String())
+	value = Column(String())
+
+# class ImageCache(Base):
+# 	__tablename__ = 'image_cache'
+# 	item_id = Column(Integer)
+# 	size_big = Column(Integer)
+# 	size_mini = Column(Integer)
+# 	image_data = Column(BLOB)
+# 	image_data_mini = Column(BLOB)
+
 
 # CREATE TABLE dbinfo (key VARCHAR, value VARCHAR);
 # CREATE TABLE items (rowid INTEGER PRIMARY KEY ASC, uuid VARCHAR, flags INTEGER, type INTEGER, parent_id INTEGER NOT NULL, ordering INTEGER);
@@ -40,7 +54,9 @@ class Apps(Base):
 # CREATE TABLE image_cache (item_id INTEGER, size_big INTEGER, size_mini INTEGER, image_data BLOB, image_data_mini BLOB);
 
 
-apps = session.query(Apps).all()
+# apps = session.query(Apps).all()
+# print(apps)
+# print(apps[0].bookmark)
 
-print(apps)
-print(apps[0].bookmark)
+# images = session.query(ImageCache).all()
+# print(images)
