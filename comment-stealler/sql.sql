7c81765ea213fccac893ae426589660dede08122
gebeto <slavik.nychkalo@gmail.com>
2020-08-20 18:41:17 +0300

    Removed sql

--- comment-stealler/sql.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO wp_comments
-(comment_post_ID, comment_author, comment_date, comment_date_gmt, comment_approved, comment_content)
-VALUES
-(398, "Гость", "2020.02.27 20:55:11", "2020.02.27 20:55:11", 1, "TEEEEEEEEE")
